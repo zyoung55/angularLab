d142c04d8d4683f5685141b43d331283af1f0464
EC2 Default User <ec2-user@ip-172-31-14-158.us-east-2.compute.internal>
2018-10-18 20:25:41 +0000

    everything but delay works

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 /*
 TODO:
 -add delay
--double click already matched (last 3 rows?)
--double click same tile (last 3 rows?)
 -make it cool when they win
 */
 
@@ -30,7 +28,7 @@ var app = angular.module('myApp', [])
     
     
     $scope.doneArray = [];
-    
+    $scope.match = false;
     $scope.numFlipped = 0;
     $scope.firstFlipped = {index: -1, pictureUrl: ""};
     $scope.secondFlipped = {index: -1, pictureUrl: ""};
@@ -46,18 +44,18 @@ var app = angular.module('myApp', [])
         $scope.secondFlipped.pictureUrl = "";
         $scope.numFlipped = 0
         $scope.doneArray = [];
-      $scope.randomizePictures(); //uncomment this line before turn in
+      //$scope.randomizePictures(); //uncomment this line before turn in
     }
     
     $scope.sleep = function (milliseconds) {
         console.log("SLEEP();");
-      var start = new Date().getTime();
-      for (var i = 0; i < 1e7; i++) {
-        if ((new Date().getTime() - start) > milliseconds){
-          break;
+        var start = new Date().getTime();
+        for (var i = 0; i < 1e7; i++) {
+            if ((new Date().getTime() - start) > milliseconds)
+                break;
         }
-      }
     }
+    
     $scope.setAllOff = function(){
         $scope.zeroZero = $scope.backPic;
         $scope.zeroOne = $scope.backPic;
@@ -82,7 +80,6 @@ var app = angular.module('myApp', [])
         if ($scope.numFlipped == 0 || $scope.numFlipped == 1) {
              ++$scope.numFlipped;
         }
-        console.log("::NumberFlipped:", $scope.numFlipped);
         callbackOne(arguments[1], arguments[2]); //what does this do?
     }
 
@@ -96,8 +93,6 @@ var app = angular.module('myApp', [])
             $scope.secondFlipped.index = arrayIndex;
             $scope.secondFlipped.pictureUrl = value;
         }
-        console.log("::firstFlipped:", $scope.firstFlipped);
-        console.log("::secondFlipped", $scope.secondFlipped);
     }
     
     $scope.setPics = function(pic){
@@ -153,161 +148,107 @@ var app = angular.module('myApp', [])
         }
     }
 
-    //TODO: add delay
     $scope.checkPicture = function() {
         console.log("checkPicture();");
-        if ($scope.numFlipped == 0) {
-            console.log("::NO pics selected");
-        } 
-        if ($scope.numFlipped == 1) {
-            console.log("::only one pic selected");
-        } 
-        if ($scope.numFlipped == 2) {
-            console.log("::two pics selected!");
-            if ($scope.firstFlipped.pictureUrl == $scope.secondFlipped.pictureUrl) {
-                console.log("::They matched! Great job!");
-                //alert("They matched! Great job!");
-                $scope.doneArray.push($scope.firstFlipped.index);
-                $scope.doneArray.push($scope.secondFlipped.index);
-                $scope.setPics($scope.matchPic);
-            }
-            else {
-     //         alert("They didn't match :(. Try again!");
-                console.log("::didn't match");
-                console.log($scope.doneArray);
-                $scope.sleep(1000);
-                
-                $scope.setPics($scope.backPic);
-
-            }
-            $scope.firstFlipped.pictureUrl = "";
-            $scope.secondFlipped.pictureUrl = "";
-            $scope.firstFlipped.index = -1;
-            $scope.secondFlipped.index = -1;
-            $scope.numFlipped = 0;
+        if ($scope.numFlipped == 0 || $scope.numFlipped == 1) 
+            return;
+         
+        if ($scope.firstFlipped.pictureUrl == $scope.secondFlipped.pictureUrl) {
+            $scope.doneArray.push($scope.firstFlipped.index);
+            $scope.doneArray.push($scope.secondFlipped.index);
+            $scope.match = true;
         }
+        else {
+            //TODO
+            console.log("!");
+            console.log("@");
+            $scope.match = false;
+        }
+
+        $scope.setFlipOff();
     }
 
+    $scope.setFlipOff = function(){
+        console.log("setFlipOff();");
+        if(!$scope.match){
+                $scope.sleep(1000);
+
+            $scope.setPics($scope.backPic);
+        }
+        $scope.firstFlipped.pictureUrl = "";
+        $scope.secondFlipped.pictureUrl = "";
+        $scope.firstFlipped.index = -1;
+        $scope.secondFlipped.index = -1;
+        $scope.numFlipped = 0;
+        console.log("setFlipOff returning");
+
+    }
+    
     $scope.flipOverCheck = function(index){
         console.log("flipOverCheck();")
-        if ($scope.numFlipped == 2) {
-            console.log("::2 already flipped homie")
+      
+        if ($scope.numFlipped == 2 || $scope.firstFlipped.index == index)
             return false;
-        }
-        if($scope.firstFlipped.index == index){
-            console.log("::" + index + " clicked again");
-            return false;
-        }
-        console.log($scope.doneArray);
         for(var i = 0; i < $scope.doneArray.length; i++){
             console.log(i + ":: " + $scope.doneArray[i]);
             if(index == $scope.doneArray[i])
                 return false;
         }
-        console.log("::ret true")
         return true;
     }
     
     $scope.flipOver = function(index) {
         console.log("flipOver(" + index + ");")
-        
         var arrIndex = Math.floor(index/10%10)*4 + Math.floor(index%10);
         
-        if(!$scope.flipOverCheck(index))
+        if(!$scope.flipOverCheck(arrIndex))
             return;
         
-    
-    
-        if (index == "00") {
-            $scope.zeroZero = $scope.array[arrIndex];//0];
-            $scope.incrementNumFlipped($scope.addToFlippedScopeValues, arrIndex, $scope.array[arrIndex]);//0, $scope.array[0]);
-        }
-        if (index == "01") {
+        if (index == "00") 
+            $scope.zeroZero = $scope.array[0];
+        if (index == "01") 
             $scope.zeroOne = $scope.array[1];
-            $scope.incrementNumFlipped($scope.addToFlippedScopeValues, 1, $scope.array[1]);
-        }
-        
-        if (index == "02") {
+        if (index == "02") 
             $scope.zeroTwo = $scope.array[2];
-            $scope.incrementNumFlipped($scope.addToFlippedScopeValues, 2, $scope.array[2]);
-        }
-        
-        if (index == "03") {
+        if (index == "03") 
             $scope.zeroThree = $scope.array[3];
-            $scope.incrementNumFlipped($scope.addToFlippedScopeValues, 3, $scope.array[3]);
-        }
-        
-        if (index == "10") {
+        if (index == "10")
             $scope.oneZero = $scope.array[4];
-            $scope.incrementNumFlipped($scope.addToFlippedScopeValues, 4, $scope.array[4]);
-        }
-        
-        if (index == "11") {
+        if (index == "11") 
             $scope.oneOne = $scope.array[5];
-            $scope.incrementNumFlipped($scope.addToFlippedScopeValues, 5, $scope.array[5]);
-        }
-        
-        if (index == "12") {
+        if (index == "12") 
             $scope.oneTwo = $scope.array[6];
-            $scope.incrementNumFlipped($scope.addToFlippedScopeValues, 6, $scope.array[6]);
-        }
-        
-        if (index == "13") {
+        if (index == "13") 
              $scope.oneThree = $scope.array[7];
-            $scope.incrementNumFlipped($scope.addToFlippedScopeValues, 7, $scope.array[7]);
-        }
-        
-        if (index == "20") {
+        if (index == "20") 
             $scope.twoZero = $scope.array[8];
-            $scope.incrementNumFlipped($scope.addToFlippedScopeValues, 8, $scope.array[8]);
-        }
-        
-        if (index == "21") {
+        if (index == "21")
             $scope.twoOne = $scope.array[9];
-            $scope.incrementNumFlipped($scope.addToFlippedScopeValues, 9, $scope.array[9]);
-        }
-        
-        if (index == "22") {
+        if (index == "22")
           $scope.twoTwo = $scope.array[10];
-            $scope.incrementNumFlipped($scope.addToFlippedScopeValues, 10, $scope.array[10]);
-        }
-        
-        if (index == "23") {
+        if (index == "23")
             $scope.twoThree = $scope.array[11];
-            $scope.incrementNumFlipped($scope.addToFlippedScopeValues, 11, $scope.array[11]);
-        }
-        
-        if (index == "30") {
+        if (index == "30")
             $scope.threeZero = $scope.array[12];
-            $scope.incrementNumFlipped($scope.addToFlippedScopeValues, 12, $scope.array[12]);
-        }
-        
-        if (index == "31") {
+        if (index == "31")
             $scope.threeOne = $scope.array[13];
-            $scope.incrementNumFlipped($scope.addToFlippedScopeValues, 13, $scope.array[13]);
-        }
-        
-        if (index == "32") {
+        if (index == "32") 
             $scope.threeTwo = $scope.array[14];
-            $scope.incrementNumFlipped($scope.addToFlippedScopeValues, 14, $scope.array[14]);
-        }
-        
-        if (index == "33") {
+        if (index == "33")
             $scope.threeThree = $scope.array[15];
-            $scope.incrementNumFlipped($scope.addToFlippedScopeValues, 15, $scope.array[15]);
+        
+        $scope.incrementNumFlipped($scope.addToFlippedScopeValues, arrIndex, $scope.array[arrIndex]);//0, $scope.array[0]);
+        
+        if($scope.numFlipped == 2){
+            $scope.checkPicture();
         }
         
-        if($scope.numFlipped == 2)
-            $scope.checkPicture();
-            
-        console.log($scope.doneArray.length);
         if($scope.doneArray.length >= 16){
             $scope.win();            
         }
-        
     }
+    
     $scope.randomizePictures = function() {
-        
         console.log("randomizePictures();");
 
         var m = $scope.array.length;
@@ -334,87 +275,3 @@ var app = angular.module('myApp', [])
 
 
 
-//Todo:
-/*
-randomizePicture function.
-assign pictures to indexes.
-note: we can randomize by swapping and then get a random
-number and swap that amount of times. 
-
-do on click for pictures
-do something when the pictures match vs. don't match
-
-function when they win: 
-tell them they won and re-randomize
-
-*/
-
-
-/*
-        if (updateScopeValue == 0) {
-            $scope.zeroZero = $scope.array[updateScopeValue];
-            console.log("zeroZero Now!!!", $scope.zeroZero);
-            console.log("updateScopeValue:", updateScopeValue);
-        }
-         if (updateScopeValue == 1) {
-            $scope.zeroOne = $scope.array[updateScopeValue];
-            console.log("zeroOne Now!!!", $scope.zeroOne);
-            console.log("updateScopeValue:", updateScopeValue);
-        }
-        if (updateScopeValue == 2) {
-            $scope.zeroTwo = $scope.array[updateScopeValue];
-            console.log("zeroTwo Now!!!", $scope.zeroTwo);
-            console.log("updateScopeValue:", updateScopeValue);
-        }
-        if (updateScopeValue == 3) {
-            $scope.zeroThree = $scope.array[updateScopeValue];
-            console.log("zeroOne Now!!!", $scope.zeroThree);
-        }
-        if (updateScopeValue == 4) {
-            $scope.oneZero = $scope.array[updateScopeValue];
-            console.log("zeroOne Now!!!", $scope.oneZero);
-        }
-        if (updateScopeValue == 5) {
-            $scope.oneOne = $scope.array[updateScopeValue];
-            console.log("zeroOne Now!!!", $scope.zeroOne);
-        }
-        if (updateScopeValue == 6) {
-            $scope.oneTwo = $scope.array[updateScopeValue];
-            console.log("zeroOne Now!!!", $scope.zeroOne);
-        }
-        if (updateScopeValue == 7) {
-            $scope.oneThree = $scope.array[updateScopeValue];
-            console.log("zeroOne Now!!!", $scope.zeroOne);
-        }
-        if (updateScopeValue == 8) {
-            $scope.twoZero = $scope.array[updateScopeValue];
-            console.log("zeroOne Now!!!", $scope.zeroOne);
-        }
-        if (updateScopeValue == 9) {
-            $scope.twoOne = $scope.array[updateScopeValue];
-            console.log("zeroOne Now!!!", $scope.zeroOne);
-        }
-        if (updateScopeValue == 10) {
-            $scope.twoTwo = $scope.array[updateScopeValue];
-            console.log("zeroOne Now!!!", $scope.zeroOne);
-        }
-        if (updateScopeValue == 11) {
-            $scope.twoThree = $scope.array[updateScopeValue];
-            console.log("zeroOne Now!!!", $scope.zeroOne);
-        }
-        if (updateScopeValue == 12) {
-            $scope.threeZero = $scope.array[updateScopeValue];
-            console.log("zeroOne Now!!!", $scope.zeroOne);
-        }
-        if (updateScopeValue == 13) {
-            $scope.threeOne = $scope.array[updateScopeValue];
-            console.log("zeroOne Now!!!", $scope.zeroOne);
-        }
-        if (updateScopeValue == 14) {
-            $scope.threeTwo = $scope.array[updateScopeValue];
-            console.log("zeroOne Now!!!", $scope.zeroOne);
-        }
-        if (updateScopeValue == 15) {
-            $scope.threeThree = $scope.array[updateScopeValue];
-            console.log("zeroOne Now!!!", $scope.zeroOne);
-        }*/
