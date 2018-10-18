617ff49525fe0a9a3c7d0c640f6ca4bce7317bd4
EC2 Default User <ec2-user@ip-172-31-14-158.us-east-2.compute.internal>
2018-10-18 22:47:57 +0000

    a lot cleaner, but delay in wrong place?

--- script.js
+++ script.js
@@ -9,54 +9,40 @@ TODO:
 var app = angular.module('myApp', [])
 .controller('myCtrl', function($scope) {
 
-
     $scope.backPic = "https://my-brushes.s3.amazonaws.com/images/vector/412515/preview/bl-Background5.png?v=5";
     $scope.matchPic = "https://oceanexplorer.noaa.gov/facts/pacific-size.jpg";
     
-    $scope.array = ['https://www.nationalgeographic.com/content/dam/animals/2018/09/comedy-wildlife-awards-photos/comedy-wildlife-awards-squirel-stop.ngsversion.1537203605960.adapt.1900.1.jpg', 
-    'https://pbs.twimg.com/profile_images/378800000831249044/effb57c08b2f5783c686b589d84d2b92.jpeg', 
-    'https://hounslowurbanfarm.co.uk/wp-content/uploads/2017/03/img-animal-Willow-the-Barn-Owl.jpg',
-    'http://www.top13.net/wp-content/uploads/2016/02/animal-love-3.jpg', 
-    'http://chdanimalhusbandry.gov.in/img/13.jpg', 'https://metrouk2.files.wordpress.com/2018/06/4233a94b93ce00ce8274afe4c3b4a818.jpg?quality=80&strip=all&zoom=1&resize=644%2C427',
-    'https://img.buzzfeed.com/buzzfeed-static/static/enhanced/web03/2012/6/20/12/enhanced-buzz-13209-1340210795-17.jpg?downsize=700:*&output-format=auto&output-quality=auto', 
-    'https://cdn.theatlantic.com/assets/media/img/photo/2018/02/animals-on-the-playing-field/a01_545766130/main_900.jpg?1517515869',
+    $scope.array = [
     'https://www.nationalgeographic.com/content/dam/animals/2018/09/comedy-wildlife-awards-photos/comedy-wildlife-awards-squirel-stop.ngsversion.1537203605960.adapt.1900.1.jpg', 
+    'https://pbs.twimg.com/profile_images/378800000831249044/effb57c08b2f5783c686b589d84d2b92.jpeg', 'https://hounslowurbanfarm.co.uk/wp-content/uploads/2017/03/img-animal-Willow-the-Barn-Owl.jpg',
+    'http://www.top13.net/wp-content/uploads/2016/02/animal-love-3.jpg', 'http://chdanimalhusbandry.gov.in/img/13.jpg', 'https://metrouk2.files.wordpress.com/2018/06/4233a94b93ce00ce8274afe4c3b4a818.jpg?quality=80&strip=all&zoom=1&resize=644%2C427',
+    'https://img.buzzfeed.com/buzzfeed-static/static/enhanced/web03/2012/6/20/12/enhanced-buzz-13209-1340210795-17.jpg?downsize=700:*&output-format=auto&output-quality=auto', 
+    'https://cdn.theatlantic.com/assets/media/img/photo/2018/02/animals-on-the-playing-field/a01_545766130/main_900.jpg?1517515869', 'https://www.nationalgeographic.com/content/dam/animals/2018/09/comedy-wildlife-awards-photos/comedy-wildlife-awards-squirel-stop.ngsversion.1537203605960.adapt.1900.1.jpg', 
     'https://pbs.twimg.com/profile_images/378800000831249044/effb57c08b2f5783c686b589d84d2b92.jpeg', 'https://hounslowurbanfarm.co.uk/wp-content/uploads/2017/03/img-animal-Willow-the-Barn-Owl.jpg',
     'http://www.top13.net/wp-content/uploads/2016/02/animal-love-3.jpg', 'http://chdanimalhusbandry.gov.in/img/13.jpg', 'https://metrouk2.files.wordpress.com/2018/06/4233a94b93ce00ce8274afe4c3b4a818.jpg?quality=80&strip=all&zoom=1&resize=644%2C427',
     'https://img.buzzfeed.com/buzzfeed-static/static/enhanced/web03/2012/6/20/12/enhanced-buzz-13209-1340210795-17.jpg?downsize=700:*&output-format=auto&output-quality=auto', 
     'https://cdn.theatlantic.com/assets/media/img/photo/2018/02/animals-on-the-playing-field/a01_545766130/main_900.jpg?1517515869'];
     
-    
     $scope.doneArray = [];
-    $scope.match = false;
     $scope.numFlipped = 0;
     $scope.firstFlipped = {index: -1, pictureUrl: ""};
     $scope.secondFlipped = {index: -1, pictureUrl: ""};
+
     
-    
-    
+    /** called on load and onclick of new game button
+     * sets all pics to back pic
+     * initializes all data members to start game
+     * randomizes pictures behind each tile
+    */
     $scope.init = function(){
         console.log("init();");
-        $scope.setAllOff();
         $scope.firstFlipped.index = -1;
         $scope.firstFlipped.pictureUrl = "";
         $scope.secondFlipped.index = -1;
         $scope.secondFlipped.pictureUrl = "";
         $scope.numFlipped = 0
         $scope.doneArray = [];
-      //$scope.randomizePictures(); //uncomment this line before turn in
-    }
-    
-    $scope.sleep = function (milliseconds) {
-        console.log("SLEEP();");
-        var start = new Date().getTime();
-        for (var i = 0; i < 1e7; i++) {
-            if ((new Date().getTime() - start) > milliseconds)
-                break;
-        }
-    }
-    
-    $scope.setAllOff = function(){
+        
         $scope.zeroZero = $scope.backPic;
         $scope.zeroOne = $scope.backPic;
         $scope.zeroTwo = $scope.backPic;
@@ -73,187 +59,8 @@ var app = angular.module('myApp', [])
         $scope.threeOne = $scope.backPic;
         $scope.threeTwo = $scope.backPic;
         $scope.threeThree = $scope.backPic;
-    }
-    
-    $scope.incrementNumFlipped = function(callbackOne) {
-        console.log("incrementNumFlipped(" + "***" + ");\n")
-        if ($scope.numFlipped == 0 || $scope.numFlipped == 1) {
-             ++$scope.numFlipped;
-        }
-        callbackOne(arguments[1], arguments[2]); //what does this do?
-    }
-
-    $scope.addToFlippedScopeValues = function(arrayIndex, value) {
-        console.log("addToFlippedScopeValues(" + arrayIndex + "," + value + ");\n")
-        if ($scope.numFlipped == 1) {
-            $scope.firstFlipped.index = arrayIndex;
-            $scope.firstFlipped.pictureUrl = value;
-        }
-        if ($scope.numFlipped == 2) {
-            $scope.secondFlipped.index = arrayIndex;
-            $scope.secondFlipped.pictureUrl = value;
-        }
-    }
-    
-    $scope.setPics = function(pic){
-        console.log("setPics()");
-        $scope.setSinglePic($scope.secondFlipped.index);
-        $scope.sleep(1000);
-        //pic ='https://cdn.theatlantic.com/assets/media/img/photo/2018/02/animals-on-the-playing-field/a01_545766130/main_900.jpg?1517515869';// $scope.backPic;
-        if ($scope.firstFlipped.index == 0 || $scope.secondFlipped.index == 0) {
-            $scope.zeroZero = pic;
-        }
-        if ($scope.firstFlipped.index == 1 || $scope.secondFlipped.index == 1) {
-            $scope.zeroOne = pic;
-        }
-        if ($scope.firstFlipped.index == 2 || $scope.secondFlipped.index == 2) {
-            $scope.zeroTwo = pic;
-        }
-        if ($scope.firstFlipped.index == 3 || $scope.secondFlipped.index == 3) {
-            $scope.zeroThree = pic;
-        }
-        if ($scope.firstFlipped.index == 4 || $scope.secondFlipped.index == 4) {
-            $scope.oneZero = pic;
-        }
-        if ($scope.firstFlipped.index == 5 || $scope.secondFlipped.index == 5) {
-            $scope.oneOne = pic;
-        }
-        if ($scope.firstFlipped.index == 6 || $scope.secondFlipped.index == 6) {
-            $scope.oneTwo = pic;
-        }
-        if ($scope.firstFlipped.index == 7 || $scope.secondFlipped.index == 7) {
-            $scope.oneThree = pic;
-        }
-        if ($scope.firstFlipped.index == 8 || $scope.secondFlipped.index == 8) {
-            $scope.twoZero = pic;
-        }
-        if ($scope.firstFlipped.index == 9 || $scope.secondFlipped.index == 9) {
-            $scope.twoOne = pic;
-        }
-        if ($scope.firstFlipped.index == 10 || $scope.secondFlipped.index == 10) {
-            $scope.twoTwo = pic;
-        }
-        if ($scope.firstFlipped.index == 11 || $scope.secondFlipped.index == 11) {
-            $scope.twoThree = pic;
-        }
-        if ($scope.firstFlipped.index == 12 || $scope.secondFlipped.index == 12) {
-            $scope.threeZero = pic;
-        }
-        if ($scope.firstFlipped.index == 13 || $scope.secondFlipped.index == 13) {
-            $scope.threeOne = pic;
-        }
-        if ($scope.firstFlipped.index == 14 || $scope.secondFlipped.index == 14) {
-            $scope.threeTwo = pic;
-        }
-        if ($scope.firstFlipped.index == 15 || $scope.secondFlipped.index == 15) {
-            $scope.threeThree = pic;
-        }
-    }
-
-    $scope.checkPicture = function() {
-        console.log("checkPicture();");
-        if ($scope.numFlipped == 0 || $scope.numFlipped == 1) 
-            return;
-
-        if ($scope.firstFlipped.pictureUrl == $scope.secondFlipped.pictureUrl) {
-            $scope.doneArray.push($scope.firstFlipped.index);
-            $scope.doneArray.push($scope.secondFlipped.index);
-            $scope.match = true;
-        }
-        else {
-            $scope.match = false;
-
-        }
-        $scope.setFlipOff();
-    }
-
-    $scope.setFlipOff = function(){
-        console.log("setFlipOff();");
-        if(!$scope.match){
-            $scope.setPics($scope.backPic);
-        }
-        $scope.firstFlipped.pictureUrl = "";
-        $scope.secondFlipped.pictureUrl = "";
-        $scope.firstFlipped.index = -1;
-        $scope.secondFlipped.index = -1;
-        $scope.numFlipped = 0;
         
-    }
-    
-    $scope.flipOverCheck = function(index){
-        console.log("flipOverCheck();")
-      
-        if ($scope.numFlipped == 2 || $scope.firstFlipped.index == index)
-            return false;
-        for(var i = 0; i < $scope.doneArray.length; i++){
-            console.log(i + ":: " + $scope.doneArray[i]);
-            if(index == $scope.doneArray[i])
-                return false;
-        }
-        return true;
-    }
-    
-    $scope.setSinglePic = function(index){
-        console.log("setSinglePic(" + index);
-                index = Math.floor(index/10%10)*4 + Math.floor(index%10);
-
-        if (index == "00") 
-            $scope.zeroZero = $scope.array[0];
-        if (index == "01") 
-            $scope.zeroOne = $scope.array[1];
-        if (index == "02") 
-            $scope.zeroTwo = $scope.array[2];
-        if (index == "03") 
-            $scope.zeroThree = $scope.array[3];
-        if (index == "10")
-            $scope.oneZero = $scope.array[4];
-        if (index == "11") 
-            $scope.oneOne = $scope.array[5];
-        if (index == "12") 
-            $scope.oneTwo = $scope.array[6];
-        if (index == "13") 
-             $scope.oneThree = $scope.array[7];
-        if (index == "20") 
-            $scope.twoZero = $scope.array[8];
-        if (index == "21")
-            $scope.twoOne = $scope.array[9];
-        if (index == "22")
-          $scope.twoTwo = $scope.array[10];
-        if (index == "23")
-            $scope.twoThree = $scope.array[11];
-        if (index == "30")
-            $scope.threeZero = $scope.array[12];
-        if (index == "31")
-            $scope.threeOne = $scope.array[13];
-        if (index == "32") 
-            $scope.threeTwo = $scope.array[14];
-        if (index == "33")
-            $scope.threeThree = $scope.array[15];
-    }
-    
-    $scope.flipOver = function(index) {
-        console.log("flipOver(" + index + ");")
-        var arrIndex = Math.floor(index/10%10)*4 + Math.floor(index%10);
-        
-        if(!$scope.flipOverCheck(arrIndex))
-            return;
-         $scope.incrementNumFlipped($scope.addToFlippedScopeValues, arrIndex, $scope.array[arrIndex]);//0, $scope.array[0]);
-        
-        $scope.setSinglePic(index);
-       
-        if($scope.numFlipped == 2){
-            $scope.checkPicture();
-        }
-        
-        if($scope.doneArray.length >= 16){
-            $scope.win();            
-        }
-    }
-    
-    $scope.randomizePictures = function() {
-        console.log("randomizePictures();");
-
-        var m = $scope.array.length;
+        /*var m = $scope.array.length;
         var i, t;
         
         while(m){
@@ -261,14 +68,185 @@ var app = angular.module('myApp', [])
             t = $scope.array[m];
             $scope.array[m] = $scope.array[i];
             $scope.array[i] = t;
-        }
+        }*/
     }
     
+    /** called ??*/
+    $scope.sleep = function (milliseconds) {
+        console.log("SLEEP();");
+        var start = new Date().getTime();
+        for (var i = 0; i < 1e7; i++) {
+            if ((new Date().getTime() - start) > milliseconds)
+                break;
+        }
+    }
+
+    /** called from checkPicture() when two clicked DON'T match*/
+    $scope.setPics = function(){
+        $scope.sleep(1000);
+        var pic = $scope.backPic;
+        console.log("setPics()");
+        if ($scope.firstFlipped.index == 0 || $scope.secondFlipped.index == 0) 
+            $scope.zeroZero = pic;
+        if ($scope.firstFlipped.index == 1 || $scope.secondFlipped.index == 1) 
+            $scope.zeroOne = pic;
+        if ($scope.firstFlipped.index == 2 || $scope.secondFlipped.index == 2) 
+            $scope.zeroTwo = pic;
+        if ($scope.firstFlipped.index == 3 || $scope.secondFlipped.index == 3) 
+            $scope.zeroThree = pic;
+        if ($scope.firstFlipped.index == 4 || $scope.secondFlipped.index == 4) 
+            $scope.oneZero = pic;
+        if ($scope.firstFlipped.index == 5 || $scope.secondFlipped.index == 5) 
+            $scope.oneOne = pic;
+        if ($scope.firstFlipped.index == 6 || $scope.secondFlipped.index == 6) 
+            $scope.oneTwo = pic;
+        if ($scope.firstFlipped.index == 7 || $scope.secondFlipped.index == 7) 
+            $scope.oneThree = pic;
+        if ($scope.firstFlipped.index == 8 || $scope.secondFlipped.index == 8) 
+            $scope.twoZero = pic;
+        if ($scope.firstFlipped.index == 9 || $scope.secondFlipped.index == 9) 
+            $scope.twoOne = pic;
+        if ($scope.firstFlipped.index == 10 || $scope.secondFlipped.index == 10) 
+            $scope.twoTwo = pic;
+        if ($scope.firstFlipped.index == 11 || $scope.secondFlipped.index == 11) 
+            $scope.twoThree = pic;
+        if ($scope.firstFlipped.index == 12 || $scope.secondFlipped.index == 12) 
+            $scope.threeZero = pic;
+        if ($scope.firstFlipped.index == 13 || $scope.secondFlipped.index == 13) 
+            $scope.threeOne = pic;
+        if ($scope.firstFlipped.index == 14 || $scope.secondFlipped.index == 14) 
+            $scope.threeTwo = pic;
+        if ($scope.firstFlipped.index == 15 || $scope.secondFlipped.index == 15) 
+            $scope.threeThree = pic;
+    }
+
+    /** called from flipOver()
+     * alerts user they won
+    */
     $scope.win = function(){
         alert("YOU WIN!");
     }
     
+    /** called from flipOver() when numFlipped==2 
+     * if pics match:
+     *   add two indexes to donearray
+     * if not:
+     *    sets both indexes to back pic
+     * sets all data members to defaults
+    */
+    $scope.checkPicture = function() {
+        console.log("checkPicture();");
     
+        if ($scope.firstFlipped.pictureUrl == $scope.secondFlipped.pictureUrl) {
+            $scope.doneArray.push($scope.firstFlipped.index);
+            $scope.doneArray.push($scope.secondFlipped.index);
+        }
+        else {
+            $scope.setPics();
+        }
+
+        $scope.firstFlipped.pictureUrl = "";
+        $scope.secondFlipped.pictureUrl = "";
+        $scope.firstFlipped.index = -1;
+        $scope.secondFlipped.index = -1;
+        $scope.numFlipped = 0;
+    }
+    
+    /** called from flipOver() 
+     * sets clicked picture to front picture
+    */
+    $scope.setSinglePic = function(index){
+        console.log("setSinglePic(" + index + ");");
+        if (index == "0") 
+            $scope.zeroZero = $scope.array[0];
+        if (index == "1") 
+            $scope.zeroOne = $scope.array[1];
+        if (index == "2") 
+            $scope.zeroTwo = $scope.array[2];
+        if (index == "3") 
+            $scope.zeroThree = $scope.array[3];
+        if (index == "4")
+            $scope.oneZero = $scope.array[4];
+        if (index == "5") 
+            $scope.oneOne = $scope.array[5];
+        if (index == "6") 
+            $scope.oneTwo = $scope.array[6];
+        if (index == "7") 
+            $scope.oneThree = $scope.array[7];
+        if (index == "8") 
+            $scope.twoZero = $scope.array[8];
+        if (index == "9")
+            $scope.twoOne = $scope.array[9];
+        if (index == "10")
+            $scope.twoTwo = $scope.array[10];
+        if (index == "11")
+            $scope.twoThree = $scope.array[11];
+        if (index == "12")
+            $scope.threeZero = $scope.array[12];
+        if (index == "13")
+            $scope.threeOne = $scope.array[13];
+        if (index == "14") 
+            $scope.threeTwo = $scope.array[14];
+        if (index == "15")
+            $scope.threeThree = $scope.array[15];
+        if($scope.numFlipped == 2){
+            console.log("###");
+        }
+            
+    }
+    
+    /** called from flipOver() 
+     * checks if tile can be flipped
+     * cant if:
+     *  -same pic already clicked
+     *  -two pics already clicked
+     *  -already been matched
+    */
+    $scope.flipOverCheck = function(index){
+        console.log("flipOverCheck();")
+        if ($scope.numFlipped == 2 || $scope.firstFlipped.index == index)
+            return false;
+        for(var i = 0; i < $scope.doneArray.length; i++){
+            if(index == $scope.doneArray[i])
+                return false;
+        }
+        console.log("::true");
+        return true;
+    }
+    
+    /** 
+     * called from main(html on click) 
+     * 
+     * checks if can flip over
+     * sets url/index of flipped (based on numFlipped)
+     * sets single pic @ given index
+     * if 2 pics selected, call check picture
+     * if 16 elements in done array, call win
+    */
+    $scope.flipOver = function(index) {
+        console.log("flipOver(" + index + ");")
+
+        if(!$scope.flipOverCheck(index))
+            return;
+        $scope.numFlipped++;
+        if($scope.numFlipped == 1){
+             $scope.firstFlipped.pictureUrl = $scope.array[index];
+             $scope.firstFlipped.index = index;
+        }
+        else if($scope.numFlipped == 2){
+            $scope.secondFlipped.pictureUrl = $scope.array[index];
+            $scope.secondFlipped.index = index;
+        }
+        console.log("::" + $scope.numFlipped);
+        $scope.setSinglePic(index);
+        if($scope.numFlipped == 2){
+            $scope.checkPicture();
+        }
+        if($scope.doneArray.length >= 16){
+            $scope.win();            
+        }
+    }
+
 })
 
 
